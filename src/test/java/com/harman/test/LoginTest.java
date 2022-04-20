6cfe1a9656a1fe28c55bf668723e95b1176f39cf
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-20 21:06:33 +0530

    dataprovider in different class

--- src/test/java/com/harman/test/LoginTest.java
+++ src/test/java/com/harman/test/LoginTest.java
@@ -6,32 +6,13 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.harman.base.WebDriverWrapper;
-	
+import com.harman.utilities.DataUtils;
+
 public class LoginTest extends WebDriverWrapper {
-	
-	//create dataprovider with name validCredentialData()
-	//admin, pass, OpenEMR
-	//physician, physician, OpenEMR
-	
-	@DataProvider
-	public String[][] validCredentialData()
-	{
-		String[][] main = new String[2][3];
-		
-		main[0][0]="admin";
-		main[0][1]="pass";
-		main[0][2]="OpenEMR";
-		
-		main[1][0]="physician";
-		main[1][1]="physician";
-		main[1][2]="OpenEMR";
-		
-		return main;
-	}
-	
-	@Test(dataProvider = "validCredentialData")
-	public void validCredentialTest(String username,String password,String expectedTitle) {
-		
+	//will start at 9:20 PM IST
+	@Test(dataProviderClass = DataUtils.class,dataProvider = "validCredentialData")
+	public void validCredentialTest(String username, String password, String expectedTitle) {
+
 		driver.findElement(By.id("authUser")).sendKeys(username);
 		driver.findElement(By.cssSelector("#clearPass")).sendKeys(password);
 		driver.findElement(By.id("login-button")).click();
