dbde1cd6c70f662d2cb17ae7d90c62abd61f4231
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-20 19:43:15 +0530

    @before and @after method

--- src/test/java/com/harman/test/LoginTest.java
+++ src/test/java/com/harman/test/LoginTest.java
@@ -6,20 +6,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
+	WebDriver driver;
 
-	@Test
-	public void validCredentialTest() {
+	@BeforeMethod
+	public void setup() {
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get("https://demo.openemr.io/b/openemr");
+	}
 
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+
+	@Test
+	public void validCredentialTest() {
 		driver.findElement(By.id("authUser")).sendKeys("admin");
 		driver.findElement(By.cssSelector("#clearPass")).sendKeys("pass");
 		driver.findElement(By.id("login-button")).click();
@@ -27,24 +38,15 @@ public class LoginTest {
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle, "OpenEMR");
 	}
-	
-	@Test
-	public void invalidCredentialTest()
-	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		driver.get("https://demo.openemr.io/b/openemr");
 
+	@Test
+	public void invalidCredentialTest() {
 		driver.findElement(By.id("authUser")).sendKeys("john");
 		driver.findElement(By.cssSelector("#clearPass")).sendKeys("john123");
 		driver.findElement(By.id("login-button")).click();
-		
-		String actualError=driver.findElement(By.xpath("//div[contains(text(),'Invalid')]")).getText();
+
+		String actualError = driver.findElement(By.xpath("//div[contains(text(),'Invalid')]")).getText();
 		Assert.assertEquals(actualError, "Invalid username or password");
 	}
-	
-	
 
 }
