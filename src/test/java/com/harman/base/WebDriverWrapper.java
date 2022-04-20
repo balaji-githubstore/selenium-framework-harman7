114c97e1987da36dc92d53493fd8befd058688ca
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-20 21:58:04 +0530

    day 3 - framework done

--- src/test/java/com/harman/base/WebDriverWrapper.java
+++ src/test/java/com/harman/base/WebDriverWrapper.java
@@ -4,8 +4,12 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -14,9 +18,20 @@ public class WebDriverWrapper {
 	protected WebDriver driver;
 
 	@BeforeMethod
-	public void setup() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+	@Parameters({"browser"})
+	public void setup(@Optional("ch") String browserName) {
+		
+		if (browserName.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else if (browserName.equalsIgnoreCase("ff")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+ 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get("https://demo.openemr.io/b/openemr");
