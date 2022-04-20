4fd10c958bdf106db0b8337d7806d08ee753a3d5
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-20 20:03:59 +0530

    inheritance - browser config -base

--- src/test/java/com/harman/test/LoginTest.java
+++ src/test/java/com/harman/test/LoginTest.java
@@ -1,34 +1,13 @@
 package com.harman.test;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class LoginTest {
-	WebDriver driver;
-
-	@BeforeMethod
-	public void setup() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		driver.get("https://demo.openemr.io/b/openemr");
-	}
-
-	@AfterMethod
-	public void teardown() {
-		driver.quit();
-	}
-
+import com.harman.base.WebDriverWrapper;
+	
+public class LoginTest extends WebDriverWrapper {
+	
 	@Test
 	public void validCredentialTest() {
 		driver.findElement(By.id("authUser")).sendKeys("admin");
