7ffd662060c2438c621c55305369f81c16b6884c
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-20 19:35:46 +0530

    two test method

--- src/test/java/com/harman/test/LoginTest.java
+++ src/test/java/com/harman/test/LoginTest.java
@@ -1,13 +1,50 @@
 package com.harman.test;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LoginTest {
-	
-	@Test(invocationCount = 3)
-	public void validCredentialTest()
-	{
-		System.out.println("valid");
+
+	@Test
+	public void validCredentialTest() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://demo.openemr.io/b/openemr");
+
+		driver.findElement(By.id("authUser")).sendKeys("admin");
+		driver.findElement(By.cssSelector("#clearPass")).sendKeys("pass");
+		driver.findElement(By.id("login-button")).click();
+
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, "OpenEMR");
 	}
+	
+	@Test
+	public void invalidCredentialTest()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://demo.openemr.io/b/openemr");
+
+		driver.findElement(By.id("authUser")).sendKeys("john");
+		driver.findElement(By.cssSelector("#clearPass")).sendKeys("john123");
+		driver.findElement(By.id("login-button")).click();
+		
+		String actualError=driver.findElement(By.xpath("//div[contains(text(),'Invalid')]")).getText();
+		Assert.assertEquals(actualError, "Invalid username or password");
+	}
+	
+	
 
 }
