1b039b4e3ce3cab4b559c56d80056e692adee4e7
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-20 20:53:01 +0530

    dataprovider for valid login

--- src/test/java/com/harman/test/LoginTest.java
+++ src/test/java/com/harman/test/LoginTest.java
@@ -2,20 +2,42 @@ package com.harman.test;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.harman.base.WebDriverWrapper;
 	
 public class LoginTest extends WebDriverWrapper {
 	
-	@Test
-	public void validCredentialTest() {
-		driver.findElement(By.id("authUser")).sendKeys("admin");
-		driver.findElement(By.cssSelector("#clearPass")).sendKeys("pass");
+	//create dataprovider with name validCredentialData()
+	//admin, pass, OpenEMR
+	//physician, physician, OpenEMR
+	
+	@DataProvider
+	public String[][] validCredentialData()
+	{
+		String[][] main = new String[2][3];
+		
+		main[0][0]="admin";
+		main[0][1]="pass";
+		main[0][2]="OpenEMR";
+		
+		main[1][0]="physician";
+		main[1][1]="physician";
+		main[1][2]="OpenEMR";
+		
+		return main;
+	}
+	
+	@Test(dataProvider = "validCredentialData")
+	public void validCredentialTest(String username,String password,String expectedTitle) {
+		
+		driver.findElement(By.id("authUser")).sendKeys(username);
+		driver.findElement(By.cssSelector("#clearPass")).sendKeys(password);
 		driver.findElement(By.id("login-button")).click();
 
 		String actualTitle = driver.getTitle();
-		Assert.assertEquals(actualTitle, "OpenEMR");
+		Assert.assertEquals(actualTitle, expectedTitle);
 	}
 
 	@Test
